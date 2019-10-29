026a0fea047dbc9c262f94714d588caeb6e80eda
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-10-29 20:46:49 +0100

    adjust server console log

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const express = require('express');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`The server is listeing onport ${PORT}`));
+app.listen(PORT, () => console.log(`The server is listening on http://localhost:${PORT}`));
 
 app.get('/', (req, res) => res.send('API is Running'));
