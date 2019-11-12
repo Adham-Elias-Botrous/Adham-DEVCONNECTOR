3618bf63df6823b9809c4fa6eae73b231b84f4d1
Adham Elias Botrous <50738429+Adham-Elias-Botours@users.noreply.github.com>
2019-11-12 17:25:32 +0100

    finish section 10 , part 53

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -1,4 +1,11 @@
-import { GET_PROFILE, PROFILE_ERROR, CLEAR_PROFILE, UPDATE_PROFILE } from '../actions/types';
+import {
+  GET_PROFILE,
+  GET_PROFILES,
+  PROFILE_ERROR,
+  CLEAR_PROFILE,
+  UPDATE_PROFILE,
+  GET_REPOS,
+} from '../actions/types';
 
 const initialState = {
   profile: null,
@@ -20,17 +27,32 @@ export default function(state = initialState, action) {
         loading: false,
       };
 
+    case GET_PROFILES:
+      return {
+        ...state,
+        profiles: payload,
+        loading: false,
+      };
+
     case PROFILE_ERROR:
       return {
         ...state,
         error: payload,
         loading: false,
       };
+
     case CLEAR_PROFILE:
       return {
         ...state,
         profile: null,
         repos: [],
+        loading: false,
+      };
+
+    case GET_REPOS:
+      return {
+        ...state,
+        repos: payload,
         loading: false,
       };
 
